37af5d8e57e25d458146e9cee8a905742ffd2dbf
Bandal <no1msh1217@gmail.com>
2023-12-26 21:16:36 +0900

    feat: 단일 연결 리스트에 노드를 추가할 수 있다.

--- src/main/kotlin/datastructure/linear/linkedlist/SingleLinkedList.kt
+++ src/main/kotlin/datastructure/linear/linkedlist/SingleLinkedList.kt
@@ -3,4 +3,18 @@ package datastructure.linear.linkedlist
 class SingleLinkedList<T> {
     var head: Node<T>? = null
 
+    fun add(newNode: Node<T>) {
+        if (head == null) {
+            head = newNode
+            return
+        }
+
+        var lastNode: Node<T> = head!!
+
+        while (lastNode.pointer != null) {
+            lastNode = lastNode.pointer!!
+        }
+
+        lastNode.pointer = newNode
+    }
 }
--- src/test/kotlin/datastructure/linear/linkedlist/SingleLinkedListTest.kt
+++ src/test/kotlin/datastructure/linear/linkedlist/SingleLinkedListTest.kt
@@ -17,4 +17,31 @@ class SingleLinkedListTest {
         assertThat(singleLinkedList.head).isEqualTo(firstNode)
     }
 
+    @Test
+    fun `단일 연결 리스트에 노드를 추가할 때 헤드가 null이라면 헤드에 노드를 추가한다`() {
+        // given
+        val singleLinkedList = SingleLinkedList<Int>()
+        val newNode = Node(1, null)
+
+        // when
+        singleLinkedList.add(newNode)
+
+        // then
+        assertThat(singleLinkedList.head).isEqualTo(newNode)
+    }
+
+    @Test
+    fun `노드를 추가할 때 헤드에 이미 값이 있으면 헤드가 가리키는 다음 자리에 노드를 추가한다`() {
+        // given
+        val singleLinkedList = SingleLinkedList<Int>()
+        val headNode = Node(1, null)
+        val newNode = Node(2, null)
+        singleLinkedList.add(headNode)
+
+        // when
+        singleLinkedList.add(newNode)
+
+        // then
+        assertThat(singleLinkedList.head!!.pointer).isEqualTo(newNode)
+    }
 }
