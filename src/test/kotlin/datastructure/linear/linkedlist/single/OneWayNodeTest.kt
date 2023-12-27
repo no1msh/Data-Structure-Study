130710223efca80383f8ac981c010ae26e3cf490
Bandal <no1msh1217@gmail.com>
2023-12-27 16:11:24 +0900

    chore: lint 적용

--- src/test/kotlin/datastructure/linear/linkedlist/single/OneWayNodeTest.kt
+++ src/test/kotlin/datastructure/linear/linkedlist/single/OneWayNodeTest.kt
@@ -1,6 +1,5 @@
 package datastructure.linear.linkedlist.single
 
-import datastructure.linear.linkedlist.single.OneWayNode
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
